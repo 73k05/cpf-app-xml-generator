0d9274eb76c67b1fefcda47b12ce2727f00a15d0
73k05 <olivier@inkos.fr>
2020-12-07 13:44:36 +0100

    [BW][EDOF] Fix restart.py Readme

--- edofparser/server_restart.py
+++ edofparser/server_restart.py
@@ -8,5 +8,8 @@ for process in psutil.process_iter():
         process.terminate()
         break
 
+git_pull_process = Popen(["git", "pull"])
+git_pull_process.wait()
+
 print('Process not found or stopped: starting it.')
 Popen(['python3.9', '__main__.py', '--ssl', '-d', './upload', '--password', '*', '--env', 'PROD'])
