3312804cde463b96594ba51924997b88bd228174
73k05 <olivier@inkos.fr>
2020-12-07 13:40:36 +0100

    [BW][EDOF] Fix restart

--- edofparser/server_restart.py
+++ edofparser/server_restart.py
@@ -3,10 +3,10 @@ from subprocess import Popen
 import psutil
 
 for process in psutil.process_iter():
-    if process.cmdline() == ['python', '__main__.py', '--ssl', '-d', './upload', '--password', '*', '--env', 'PROD']:
+    if process.cmdline() == ['python3.9', '__main__.py', '--ssl', '-d', './upload', '--password', '*', '--env', 'PROD']:
         print('Process found. Terminating it.')
         process.terminate()
         break
 
 print('Process not found or stopped: starting it.')
-Popen(['python', '__main__.py', '--ssl', '-d', './upload', '--password', '*', '--env', 'PROD'])
+Popen(['python3.9', '__main__.py', '--ssl', '-d', './upload', '--password', '*', '--env', 'PROD'])
